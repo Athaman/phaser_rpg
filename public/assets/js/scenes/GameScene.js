1f65b2162659fae5d8d87d6b7a53bc1fe3a5fbca
Keone Martin <keonemartin@gmail.com>
2020-05-25 18:00:56 +1000

    refactor out into create functions

--- public/assets/js/scenes/GameScene.js
+++ public/assets/js/scenes/GameScene.js
@@ -10,24 +10,17 @@ class GameScene extends Phaser.Scene {
   preload() {}
 
   create() {
-    this.goldPickupAudio = this.sound.add('goldSound', {
-      loop: false,
-      volume: 0.2,
-    });
+    this.createAudio();
 
-    this.chest = new Chest(this, 300, 300, 'items', 0);
+    this.createChests();
 
-    this.player = new Player(this, 32, 32, 'characters', 0);
+    this.createPlayer();
 
-    // this.physics.add.collider(this.player, this.wall);
-    this.physics.add.overlap(
-      this.player,
-      this.chest,
-      this.collectChest,
-      null,
-      this
-    );
-    this.cursors = this.input.keyboard.createCursorKeys();
+    this.createWalls();
+
+    this.addCollisions();
+
+    this.createInput();
   }
 
   update() {
@@ -38,5 +31,40 @@ class GameScene extends Phaser.Scene {
     this.goldPickupAudio.play();
     this.events.emit('updatescore', chest.coins);
     chest.destroy();
+  }
+
+  createAudio() {
+    this.goldPickupAudio = this.sound.add('goldSound', {
+      loop: false,
+      volume: 0.2,
+    });
+  }
+
+  createChests() {
+    this.chest = new Chest(this, 300, 300, 'items', 0);
+  }
+
+  createPlayer() {
+    this.player = new Player(this, 32, 32, 'characters', 0);
+  }
+
+  createWalls() {
+    this.wall = this.physics.add.image(500, 100, 'button1');
+    this.wall.setImmovable();
+  }
+
+  addCollisions() {
+    this.physics.add.collider(this.player, this.wall);
+    this.physics.add.overlap(
+      this.player,
+      this.chest,
+      this.collectChest,
+      null,
+      this
+    );
+  }
+
+  createInput() {
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 }
